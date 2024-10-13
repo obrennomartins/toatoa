c935727158372b515ec6b9184f5f2d591bff4144
Brenno Martins <brenno.martins@jointecnologia.com.br>
2024-10-13 15:29:42 -0300

    ✨ feat: substitui o banco de dados em memória por Postgres

---
+++ src/ToAToa.DataAccess/Mappings/AtividadeMapping.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ToAToa.Domain.Entities;
+
+namespace ToAToa.DataAccess.Mappings;
+
+public class AtividadeMapping : IEntityTypeConfiguration<Atividade>
+{
+    public void Configure(EntityTypeBuilder<Atividade> builder)
+    {
+        builder.ToTable("Atividade");
+        
+        builder.HasKey(atividade => atividade.Id);
+
+        builder.Property(atividade => atividade.Descricao)
+            .IsRequired()
+            .HasColumnType("text")
+            .HasMaxLength(50);
+    }
+}
